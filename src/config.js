3bd2844bd920519f0c1baf6d5ae134d8a4414496
Blade Boles <bladerboles@gmail.com>
2020-02-22 21:03:21 -0700

    Fixed config for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000'
+  API_ENDPOINT: 'https://morning-ocean-06315.herokuapp.com'
 }
 
 
-// API_ENDPOINT: 'https://morning-ocean-06315.herokuapp.com'
+// API_ENDPOINT: 'http://localhost:8000'
