bf79deb0499b9b31e2d36ac856e708e64f70820d
Blade Boles <bladerboles@gmail.com>
2020-02-24 13:15:54 -0700

    Hopefully fixed favicon

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000'
+  API_ENDPOINT: 'https://morning-ocean-06315.herokuapp.com'
 }
 
 
 // API_ENDPOINT: 'https://morning-ocean-06315.herokuapp.com'
+// API_ENDPOINT: 'http://localhost:8000'
